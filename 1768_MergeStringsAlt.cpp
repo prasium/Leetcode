3193c6070203160d1399fd56c7058c23e0296f67
Prasun Verma <prasunverma12@gmail.com>
2022-03-21 19:05:37 +0530

    new pro

---
+++ 1768_MergeStringsAlt.cpp
@@ -0,0 +1,40 @@
+"""
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+Input: word1 = abc, word2 = pqr
+Output: apbqcr
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+"""
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string res;
+        int m = word1.length();
+        int n = word2.length();
+        int i=0,j=0,k=0;
+        while(i<m&&j<n)
+        {
+            if(k%2==0)
+            res+=word1[i++];
+            else
+            res+=word2[j++];
+            k++;
+        }
+        while(i<m)
+        {
+            res+=word1[i++];
+            k++;
+        }
+        while(j<n)
+        {
+            res+=word2[j++];
+            k++;
+        }
+        return res;
+        
+    }
+};
