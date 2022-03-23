51b59779c5b28980ca33a583c18a4823d8873747
Prasun Verma <prasunverma12@gmail.com>
2022-03-23 13:42:37 +0530

    new pro

---
+++ 1290_BinaryToLLint.cpp
@@ -0,0 +1,28 @@
+"""
+1290. Convert Binary Number in a Linked List to Integer
+Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+
+Return the decimal value of the number in the linked list.
+"""
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int dec = 0;
+        while(head!=NULL)
+        {
+           dec= dec*2+head->val;
+            head=head->next;
+        }
+        return dec;
+    }
+};
