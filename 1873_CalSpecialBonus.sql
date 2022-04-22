54485b43601e267aa5ab1160624032fe8cb5004d
Prasun Verma <prasunverma12@gmail.com>
2022-04-22 22:24:43 +0530

    SQL

---
+++ 1873_CalSpecialBonus.sql
@@ -0,0 +1,24 @@
+"""
+Table: Employees
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| name        | varchar |
+| salary      | int     |
++-------------+---------+
+employee_id is the primary key for this table.
+Each row of this table indicates the employee ID, employee name, and salary.
+
+ 
+
+Write an SQL query to calculate the bonus of each employee. The bonus of an employee is 100% of their salary if the ID of the employee is an odd number and the employee name does not start with the character 'M'. The bonus of an employee is 0 otherwise.
+
+Return the result table ordered by employee_id.
+"""
+# Write your MySQL query statement below
+
+SELECT EMPLOYEE_ID, 
+IF(EMPLOYEE_ID%2=1 AND NAME NOT LIKE 'M%', SALARY, 0) AS BONUS
+FROM EMPLOYEES;
