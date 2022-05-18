a493f8a973af07c086d33282e633552db18afa56
Prasun Verma <prasunverma12@gmail.com>
2022-05-18 14:55:22 +0530

    changes

--- 15_3sum.cpp
+++ 15_3sum.cpp
@@ -11,3 +11,33 @@ Input: nums = [-1,0,1,2,-1,-4]
 Output: [[-1,-1,2],[-1,0,1]]
 
 """
+class Solution {
+public:
+    unordered_map<int,int> ump;
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        int n = nums.size();
+        if(n<3)
+            return res;
+      
+        sort(nums.begin(), nums.end());
+        for(int i=0; i<n; i++)
+            ump[nums[i]]=i;
+        
+        for(int i=0; i<n-2; i++)
+        {
+            if(nums[i]>0){
+                break;
+            }
+            for(int j=i+1; j<n-1; j++){
+                int reqd = -1*(nums[i]+nums[j]);
+                if(ump.count(reqd) && ump.find(reqd)->second>j){
+                    res.push_back({nums[i], nums[j], reqd});
+                }
+                j=ump.find(nums[j])->second;
+            }
+            i = ump.find(nums[i])->second;
+        }
+        return res;
+    }
+};
