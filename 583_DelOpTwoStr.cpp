6f0d4d8a33d1d1729976e36d683c1f23d00ec5bd
Prasun Verma <prasunverma12@gmail.com>
2022-06-15 00:27:50 +0530

    dp

--- 583_DelOpTwoStr.cpp
+++ 583_DelOpTwoStr.cpp
@@ -14,18 +14,19 @@ Explanation: You need one step to make "sea" to "ea" and another step to make "e
 """
 class Solution {
 public:
-   vector<vector<int> >dp;
 int minDistance(string& word1, string& word2) {
-	// dp[i][j] will denote minimum steps required to equalize word1[i:end] and word[2:j:end]
-	dp.resize(size(word1) + 1, vector<int>(size(word2) + 1, 1000));
-	return solve(word1, word2, 0, 0);
-}
-int solve(string &w1, string &w2, int i, int j) {
-	if(i == size(w1) && j == size(w2)) return 0;
-	if(i == size(w1) || j == size(w2)) return max(size(w1) - i, size(w2) - j);
-	if(dp[i][j] != 1000) return dp[i][j];  // directly return stored answer if already computed before
-	if(w1[i] == w2[j]) return solve(w1, w2, i + 1, j + 1);
-	return dp[i][j] = 1 + min(solve(w1, w2, i + 1, j), solve(w1, w2, i, j + 1));
-
+    int m = word1.length(), n =word2.length();
+    int dp [m+1][n+1];
+    for(int i=0; i<=m ; i++){
+        for(int j=0; j<=n; j++){
+            if(i==0 ||j==0)
+                dp[i][j]= i+j;
+            else if(word1[i-1]==word2[j-1])
+                dp[i][j] = dp[i-1][j-1];
+            else 
+                dp[i][j] = 1+ min(dp[i-1][j], dp[i][j-1]);
+        }
     }
+    return dp[m][n];
+}
 };
