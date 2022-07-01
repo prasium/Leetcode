13e8b48e6ed1b33de2cd0f4039e96f51b8559349
Prasun Verma <prasunverma12@gmail.com>
2022-07-01 22:57:09 +0530

    binary tree

---
+++ 226_InvertBinaryTree.cpp
@@ -0,0 +1,32 @@
+"""
+Given the root of a binary tree, invert the tree, and return its root.
+
+ 
+"""
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+       if(root==NULL)
+           return root;
+        
+        invertTree(root->left);
+        invertTree(root->right);
+        
+        TreeNode *temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+        return root;
+        
+    }
+};
